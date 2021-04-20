5e901914c547d7711d1e4c0d19cc38562a63f856
Arkadiy14 <47496737+Arkadiy14@users.noreply.github.com>
2021-04-20 14:35:43 +0300

    Update search.php

--- search.php
+++ search.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Ideas</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
+    <meta charset="utf-8">
+    <title>Ideas</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -12,24 +12,26 @@
   <button name="button" type="submit">Find</button>
 </form>
 <div class="a"><a href="https://ideashouse.herokuapp.com/">Back</a></div>
+	
 <?php
 if(isset($_POST['button']) && !empty($_POST['query'])) {
-	$link = pg_connect("CONNECT");
-	$words = explode(' ', $_POST['query']);
+    $link = pg_connect("CONNECT");
+    $words = explode(' ', $_POST['query']);
 
-	function newString($a) {
-		return '%'.$a.'%';
-	}
+    function newString($a) {
+	return '%'.$a.'%';
+    }
 
-	$words = array_map('newString', $words);
-	$words = implode(' ', $words);
-	$query = pg_prepare($link, "search_query", 'SELECT * FROM ideas WHERE idea LIKE $1');
-	$execute = pg_execute($link, "search_query", array($words));
-	while($result = pg_fetch_assoc($execute)) {
+    $words = array_map('newString', $words);
+    $words = implode(' ', $words);
+    $query = pg_prepare($link, "search_query", 'SELECT * FROM ideas WHERE idea LIKE $1');
+    $execute = pg_execute($link, "search_query", array($words));
+    while($result = pg_fetch_assoc($execute)) {
         echo '<div class="idea">'.$result['idea'].'<div class="description">'.$result['description'].'</div></div>';
     }
 }
 ?>
+	
 <style>
 * {
     box-sizing: border-box;
@@ -70,7 +72,7 @@ button {
 }
 
 .idea {
-	width: 600px;
+    width: 600px;
     margin: 0 auto;
     font-size: 20px;
     margin-top: 30px;
@@ -78,18 +80,18 @@ button {
 }
 
 .description {
-	font-size: 14px;
+    font-size: 14px;
 }
 
 .a {
-	text-align: center;
-	margin-top: 17px;
-	margin-bottom: -25px;
+    text-align: center;
+    margin-top: 17px;
+    margin-bottom: -25px;
 }
 
 a {
-	font-size: 15px;
-	text-decoration: none;
+    font-size: 15px;
+    text-decoration: none;
 }
 </style>
 </body>
