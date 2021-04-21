259bebe53239d7e697e15aae6a70a3e54367834b
Arkadiy14 <47496737+Arkadiy14@users.noreply.github.com>
2021-04-21 16:33:57 +0300

    now topics can be used

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 $link = pg_connect("CONNECT");
 
+$topics = [
+    'art' => ['movie' => 'movie', 'book' => 'book', 'literature' => 'literature', 'film' => 'film', 'theatre' => 'theatre', 'dance' => 'dance', 'painting' => 'painting', 'music' => 'music'], 
+
+    'science' => ['biology' => 'biology', 'chemistry' => 'chemistry', 'maths' => 'maths', 'math' => 'math', 'linguistics' => 'linguistics', 'physics' => 'physics', 'geography' => 'geography'],
+
+    'programming' => ['program' => 'program', 'script' => 'script', 'bot' => 'bot', 'code' => 'code', 'array' => 'array', 'value' => 'value'],
+];// here are our topics
+
 if(isset($_POST['button'])) {
     $query = pg_prepare($link, "my_query", 'INSERT INTO ideas (idea, description) VALUES ($1, $2)');
     $info = explode(':', $_POST['idea']);
@@ -8,7 +16,30 @@ if(isset($_POST['button'])) {
     if(isset($info[0]) && isset($info[1])) {
 	$idea = $info[0];
 	$description = $info[1];
-        $result = pg_execute($link, "my_query", array($idea, $description));
+	$idea_words = str_word_count($idea, 1); // here we have everything from user's idea
+	$descr_words = str_word_count($description, 1); // here we have everything from user's description
+
+        $values = array_merge($topics['art'], $topics['science'], $topics['programming']);
+
+        $same_idea = array_intersect($idea_words, $values); // looking for identical words using idea
+        $same_descr = array_intersect($descr_words, $values); // looking for identical words using descr
+
+        $idea_word = explode(' ', implode(' ', $same_idea));
+        $descr_word = explode(' ', implode(' ', $same_descr));
+
+        $topic = 0; // main topic
+	    
+	foreach($topics as $k => $v) {
+	    if($topics[$k][$idea_word[0]] && $topics[$k][$descr_word[0]]) {
+	        $topic = $k;
+	        break;
+            }else {
+	        $topic = 'other';
+	    }
+
+        }
+
+	$result = pg_execute($link, "my_query", array($idea, $description, $topic));
         header('location: https://'.$_SERVER['HTTP_HOST']);	
     }else {
 	header('location: https://'.$_SERVER['HTTP_HOST']);	
@@ -35,7 +66,7 @@ if(isset($_POST['button'])) {
 $query = pg_query($link, "SELECT * FROM ideas;");
 
 while($result = pg_fetch_assoc($query)) {
-    echo '<div class="idea">'.$result['idea'].'<div class="description">'.$result['description'].'</div></div>';
+echo '<div class="idea">'.$result['idea'].'<div class="topic">'.$result['topic'].'</div><div class="description">'.$result['description'].'</div></div>';
 }
 ?>
 	
@@ -99,6 +130,12 @@ button {
 a {
     font-size: 15px;
     text-decoration: none;
+}
+	
+.topic {
+    font-size: 15px;
+    float: right;
+    margin-top: 3px;
 }
 </style>
 </body>
