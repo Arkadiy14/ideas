f1b3b6dd9aee583162444f2da88d428fe2e54d45
Arkadiy14 <47496737+Arkadiy14@users.noreply.github.com>
2021-04-26 16:24:36 +0300

    Update search.php

--- search.php
+++ search.php
@@ -5,64 +5,6 @@
  <title>Ideas</title>
  <link rel="preconnect" href="https://fonts.gstatic.com">
  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-</head>
-<body>
-<form action="<?=$_SERVER['SCRIPT_NAME'];?>" method="post">
-<select name="topics">
- <option value="all">All</option>
- <option value="art">Art</option>
- <option value="science">Science</option>
- <option value="programming">Programming</option>
- <option value="other">Other</option>
-</select>
-  <input class="search" name="query" placeholder="Type your query" type="search">
-  <button name="button" type="submit">Find</button>
-</form>
-<div class="a"><a href="https://ideashouse.herokuapp.com/">Back</a></div>
-
-<?php
-if(isset($_POST['button']) && !empty($_POST['query'])) {
-    $link = pg_connect("CONNECT");
-    $words = explode(' ', $_POST['query']);
-    $topic = $_POST['topics'];
-
-    function newString($a) {
-	return '%'.$a.'%';
-    }
-
-    function low($a) {
-    	return strtolower($a);
-    }
-
-    $low = array_map('low', $words);
-    $string = implode(' ', array_map('newString', $low));
-
-    if($topic == 'all') {
-        $query = pg_prepare($link, "search_query", 'SELECT * FROM ideas WHERE (LOWER(idea) LIKE $1) 
-    	OR (LOWER(description) LIKE $1)');
-        $execute = pg_execute($link, "search_query", array($string));
-
-    }else {
-    	$query = pg_prepare($link, "search_query", 'SELECT * FROM ideas WHERE (topic = $1) AND 
-    	(LOWER(idea) LIKE $2) OR (topic = $1) AND (LOWER(description) LIKE $2)');
-    	$execute = pg_execute($link, "search_query", array($topic, $string));
-
-    }
-
-    $message = '<h2 align="center">Nothing found!</h2>';
-
-    while($result = pg_fetch_assoc($execute)) {
-        echo '<div class="idea">'.$result['idea'].'<div class="topic">'.$result['topic'].'</div>
-        <div class="description">'.$result['description'].'</div></div>';
-        $message = '';
-    }
-
-    if(!$result) {
-        echo $message;
-    }
-}
-?>
-
 <style>
 * {
     box-sizing: border-box;
@@ -151,5 +93,63 @@ h2 {
     padding-top: 30px;
 }
 </style>
+</head>
+<body>
+<form action="<?=$_SERVER['SCRIPT_NAME'];?>" method="post">
+<select name="topics">
+ <option value="all">All</option>
+ <option value="art">Art</option>
+ <option value="science">Science</option>
+ <option value="programming">Programming</option>
+ <option value="other">Other</option>
+</select>
+  <input class="search" name="query" placeholder="Type your query" type="search">
+  <button name="button" type="submit">Find</button>
+</form>
+<div class="a"><a href="https://ideashouse.herokuapp.com/">Back</a></div>
+
+<?php
+if(isset($_POST['button']) && !empty($_POST['query'])) {
+    $link = pg_connect("CONNECT");
+    $words = explode(' ', $_POST['query']);
+    $topic = $_POST['topics'];
+
+    function newString($a) {
+	return '%'.$a.'%';
+    }
+
+    function low($a) {
+    	return strtolower($a);
+    }
+
+    $low = array_map('low', $words);
+    $string = implode(' ', array_map('newString', $low));
+
+    if($topic == 'all') {
+        $query = pg_prepare($link, "search_query", 'SELECT * FROM ideas WHERE (LOWER(idea) LIKE $1) 
+    	OR (LOWER(description) LIKE $1)');
+        $execute = pg_execute($link, "search_query", array($string));
+
+    }else {
+    	$query = pg_prepare($link, "search_query", 'SELECT * FROM ideas WHERE (topic = $1) AND 
+    	(LOWER(idea) LIKE $2) OR (topic = $1) AND (LOWER(description) LIKE $2)');
+    	$execute = pg_execute($link, "search_query", array($topic, $string));
+
+    }
+
+    $message = '<h2 align="center">Nothing found!</h2>';
+
+    while($result = pg_fetch_assoc($execute)) {
+        echo '<div class="idea">'.$result['idea'].'<div class="topic">'.$result['topic'].'</div>
+        <div class="description">'.$result['description'].'</div></div>';
+        $message = '';
+    }
+
+    if(!$result) {
+        echo $message;
+    }
+}
+?>
+
 </body>
 </html>
